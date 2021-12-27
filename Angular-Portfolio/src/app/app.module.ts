2e231b1c6a850ddb4d0b8b53f928f075b1d0410a
Mayank-Khurmaim <mayankkhurmai8@gmail.com>
2021-12-27 22:40:19 +0530

    Angular Portfolio setup

--- Angular-Portfolio/src/app/app.module.ts
+++ Angular-Portfolio/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
